f9411b4782ebab9bc6e3e6dff782dad0f84da295
= <=>
2023-01-31 00:45:45 -0700

    implement win game logic and confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { nanoid } from 'nanoid';
 import { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
+import { useWindowSize } from 'react-use';
 import './App.css';
 import Dice from './components/Dice';
 
 function App() {
   const [dice, setDice] = useState(diceInit());
-  const [gameWon, setGameWon] = useState(false);
+  const [tenzies, setTenzies] = useState(false);
+  const {width, height} = useWindowSize();
   
+  // return array of 10 randomized dice objects
   function diceInit() {
     const initialDice = [];
     for (let i = 0; i < 10; i++) {
@@ -37,39 +41,42 @@ function App() {
 
   // dice array state update: if die is not 'locked', then roll a new value
   function rollDice() {
-    setDice(oldDice => oldDice.map(
-      die => ({
-        ...die,
-        value: die.locked ? die.value : randomRoll()
-      })
-    ))
+    if (tenzies) {
+      setDice(diceInit)
+      setTenzies(false)
+    } else (
+      setDice(oldDice => oldDice.map(
+        die => ({
+          ...die,
+          value: die.locked ? die.value : randomRoll()
+        })
+      ))
+    )
   }
   
+  // win game if all dice match values, and all dice are locked
   useEffect(() => {
-    // check if all dice match values
+    const allLocked = dice.every(die => die.locked === true)
     const val = dice[0].value;
-    let winCheck = true
-    for (let i = 1; i < dice.length; i++) {
-      if (val !== dice[i].value) {
-        winCheck = false;
-        break
-      }
-    }
-    // winCheck === gameWon ? null : setGameWon(winCheck)
-    winCheck === gameWon ? null : console.log('game win!');
+    const allSameValue = dice.every(die => die.value === val)
+    allLocked && allSameValue ? setTenzies(true) : null
   }, [dice])
 
   return (
     <main>
       <h1>Tenzies</h1>
       <p>
-        Roll until all dice are the same.<br />Click each die to freeze it at its
-        current value between rolls.
+        Roll until all dice are the same.
+        <br />
+        Click each die to freeze it at its current value between rolls.
+        <br />
+        Lock all dice to the same value to win!
       </p>
       <Dice dice={dice} toggleLock={toggleLock} />
       <button className="button--roll" onClick={rollDice} type="button">
-        Roll
+        {tenzies ? '🔄 New game' : '🎲 Roll'}
       </button>
+      {tenzies ? <Confetti width={width} height={height} /> : null}
     </main>
   );
 }
